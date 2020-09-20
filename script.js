0aca15ae139641c0bc5225d0bc31941ce10a0d5e
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-09-20 19:59:39 -0400

    updated script.js with prompt, if/else statement and alert

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordLength = 0;
 
 // Write password to the #password input
 function writePassword() {
+  // section A
+  // prompt user to enter length of pw (variable and prompt)
+  var pwLength = prompt("Please Enter Length of Desired Password");
+  // let user know in prompt that length should be between 8 - 128 (prompt)
+  var pwLngthRqrmt = prompt(
+    "Password Length Must Be Between 8 - 128 Characters"
+  );
+  // validate that value is between 8 - 128 (if else statement)
+  if (pwLnghthRqrmt) {
+    alert("Your Password Meets the Password Length Requirement!");
+  } else {
+    alert("Your Password Must Be Between 8 - 128 Characters!");
+  }
+  // if validation fails, user needs to be alerted (alert)
+  alert("Your Password Could Not Be Validated - Please Try Again!");
+  // Section B
+
+  // Section C
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
